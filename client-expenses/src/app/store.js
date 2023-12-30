45dd1762397029b00b0ccaeafd8e57e8605fa402
Matthew Ng <matthewmng1@gmail.com>
2023-12-29 21:53:20 -0500

    update store

--- client-expenses/src/app/store.js
+++ client-expenses/src/app/store.js
@@ -2,12 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "../features/auth/authSlice"
 import expensesReducer from "../features/expenses/expensesSlice"
+import usersReducer from "../features/users/usersSlice"
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
-    expenses: expensesReducer,
   },
   middleware: getDefaultMiddleware => 
     getDefaultMiddleware().concat(apiSlice.middleware),
