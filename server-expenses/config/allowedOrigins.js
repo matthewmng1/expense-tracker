f7c13b28bd4e55788be11ab11db561b2f1b60d78
Matthew Ng <matthewmng1@gmail.com>
2024-01-03 09:51:14 -0700

    update cors

--- server-expenses/config/allowedOrigins.js
+++ server-expenses/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   'https://expense-tracker-b627.onrender.com', 
+  'https://server-expense-tracker.onrender.com',
   'http://127.0.0.1:5500',
   'http://192.168.1.32:3000',
   'http://localhost:3001',
