71f9ec77f484978b45cd12920eb946cfef3456ba
Matthew Ng <matthewmng1@gmail.com>
2024-01-04 09:33:12 -0700

    update server.js

--- server-expenses/server.js
+++ server-expenses/server.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 
+const path = require('path')
 const app = require('./app');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
+
+app.use(express.static('build'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client-front', 'build', 'index.html'))
+})
 
 mongoose.connection.once('open', () => {
   console.log('Connected to MongoDB');
