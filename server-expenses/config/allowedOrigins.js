effb1d154c26811d1bc9a00ac1588d763e63a92f
Matthew Ng <matthewmng1@gmail.com>
2024-01-10 11:10:12 -0700

    update for vercel

--- server-expenses/config/allowedOrigins.js
+++ server-expenses/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
   'https://expense-tracker-b627.onrender.com', 
   'https://server-expense-tracker.onrender.com',
+  'https://expense-tracker-server-hazel.vercel.app',
+  
   'http://127.0.0.1:5500',
   'http://192.168.1.32:3000',
   'http://localhost:3001',
